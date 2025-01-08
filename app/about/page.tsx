5eddc245e03ed2b67f188f87b517347fcd20ae2f
Anne <anne12080901@gmail.com>
2025-01-08 15:06:22 -0500

    added animated description text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,30 +1,25 @@
 "use client"
 import gsap from "gsap";
-import { useRef } from "react";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { NavBar } from "@/components/navbar";
-import "./about.css"
+import TextPlugin from "gsap/TextPlugin";
+import { useEffect, useRef } from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { NavBar } from "@/components/navbar";
+import "./about.css"
 
-gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerPlugin(TextPlugin);
 
 export default function About() {
-    const container = useRef(null);
-    // const tl = useRef<any| null>(null);
-    
+    const textRef = useRef<HTMLDivElement>(null);
 
-    // useGSAP(() => {
-    //     ScrollTrigger.create({
-    //         trigger: ".page-1",
-    //         snap: {
-    //           snapTo: 1, // Snap to the closest half page
-    //           duration: 0.75, // Animation duration for the snap
-    //           ease: "power4.in", // Easing for the snap
-    //         },
-    //         scrub: true
-    //       });
-    // }, {scope: container})
+    useEffect(() => {
+        const tl = gsap.timeline({repeat: 0})
+        tl.add(gsap.timeline({repeat: 0}).to('#name', {duration: 1, text: "♥ Name: Anne Sun"}))
+        tl.add(gsap.timeline({repeat: 0}).to('#education', {duration: 1, text: "♥ Education: Term 2B Computer Science student at the University of Waterloo!"}))
+        tl.add(gsap.timeline({repeat: 0}).to('#expertise', {duration: 1, text: "♥ Expertise: Full-stack development, game development, ML/DL"}))
+        tl.add(gsap.timeline({repeat: 0}).to('#loves', {duration: 1, text: "♥ What I Love:  yoga, my 5 year old cat, digital art, swimming"}))
+    })
 
     return (
         <div className="flex flex-row justify-evenly align-items">
@@ -38,18 +33,16 @@ export default function About() {
                 {/* <img className="cat" src="./images/cat-portfolio-img.jpg"></img> */}
             <div className="about-me-container">
                 <h1 className="text-3xl">About Me</h1>
-                <div className="description-container">
-                    <p>♥ Name: Anne Sun </p>
-                    <p>♥ Education: Second year Computer Science student at the University of Waterloo!</p>
-                    <p>♥ Expertise: Full-stack development, game development and machine learning/deep learning</p>
-                    <p>♥ Values: I'm passionate about empowering women and underrepresented genders in STEM 
-                        through education and providing equitable oppurtunities in my role as a WiCS executive.</p>
-                    <p>♥ What I Love:  yoga, my 5 year old cat, digital art, swimming</p>
+                <div className="description-container" ref={textRef}>
+                    <p id="name"></p>
+                    <p id="education"></p>
+                    <p id="expertise"></p>
+                    <p id="loves"></p>
                 </div>
                 
                 <div className="icon-container">
-                    <div className="icon"><GitHubIcon fontSize="large"/></div>
-                    <div className="icon"><LinkedInIcon fontSize="large"/></div>
+                    <div className="icon"><a href="https://github.com/AnneSun1" target="_blank"><GitHubIcon fontSize="large"/></a></div>
+                    <div className="icon"><a href="https://www.linkedin.com/in/sunanne/" target="_blank"><LinkedInIcon fontSize="large"/></a></div>
                     <button>Resume</button>
                 </div>
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,9 +6,10 @@ import './navbar.css';
 export function NavBar() {
     return (
         <div className="navbar">
+            <div className="m-4"><Link href="/">Home</Link></div>
             <div className="m-4"><Link href="/about">About Me</Link></div>
             <div className="m-4"><Link href="/projects">Projects</Link></div>
-            <div className="m-4"><Link href="/">Home</Link></div>
+            
         </div>
     )
 }
